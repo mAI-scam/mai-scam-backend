bb4a137cc6cceb67854f5a5341dd658d119f13e2
Shaun Liew Xin hong <shaunliew20@gmail.com>
2025-07-16 19:24:40 +0800

    update the dependancy so that it is able to use the latest version of fastapi and also .env to load the yaml instead of using the manual command export

--- setting.py
+++ setting.py
@@ -1,14 +1,18 @@
 import os
 import yaml
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 def Setting() -> dict:
-    # Step 1: Determine environment
-    env_name = os.getenv("APP_ENV").lower()
+    # Step 1: Load environment variables from .env file (always override existing values)
+    load_dotenv(override=True)
+    
+    # Step 2: Determine environment
+    env_name = os.getenv("APP_ENV", "uat").lower()
     config_path = Path(__file__).resolve().parent / "env" / f"{env_name}.yaml"
 
-    # Step 2: Load YAML if it exists
+    # Step 3: Load YAML if it exists
     config = {}
     if config_path.exists():
         with open(config_path, "r") as f:
